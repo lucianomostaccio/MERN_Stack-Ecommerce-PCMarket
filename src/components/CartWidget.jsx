9b92fee71d0452a75fe074b32c14b777b1ea57cf
Luciano Mostaccio <lucianomostaccio@gmail.com>
2023-06-19 19:37:01 -0300

    eliminación de archivos innecesarios y separación de css a App.css

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -2,26 +2,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 function CartWidget({ quantity }) {
-  const cartStyle = {
-    listStyle: "none", // Elimina el estilo de la lista
-    marginLeft: "2rem",
-  };
-
-  const quantityStyle = {
-    marginLeft: "3px",
-    fontStyle: "normal",
-  };
-
-  const linkStyle = {
-    textDecoration: "none",
-  };
 
   return (
-    <li className="carritoNav" style={cartStyle}>
-      <a span id="cart-icon" style={linkStyle} href="#sectionCarrito">
+    <li className="carritoNav">
+      <a span id="cart-icon" href="#sectionCarrito">
         CARRITO
         <FontAwesomeIcon icon={faShoppingCart} />
-        <i className="fa-solid" id="cantidadCarrito" style={quantityStyle}>
+        <i className="fa-solid" id="cantidadCarrito">
           {(quantity = 1)}
         </i>
       </a>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -25,7 +25,7 @@ function NavBar() {
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
+            id="nav"
             navbarScroll
           >
             <Nav.Link href="../public/index.html">Home</Nav.Link>
